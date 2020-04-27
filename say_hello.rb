5cdf4cf1246b8bf58a6e92cc5c0c89871f1314fa
Erik Weir <erik.weir@key.me>
2020-04-27 19:52:53 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 def say_hello(name = "Ruby Programmer")
   puts "Hello #{name}!"
 end
 say_hello
+=======
+def say_hello("Gabriela")
+end
+>>>>>>> 2da0c91f4551f44034f6af9e04a5ac19f7a2c917
